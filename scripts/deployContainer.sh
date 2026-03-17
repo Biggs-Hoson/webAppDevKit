60fc40a904246239f1e2963371ab995897b83a99
William Lewin <william.lewin@outlook.com>
2026-03-17 23:58:04 +0000

    Continuing with devops infra.

--- scripts/deployContainer.sh
+++ scripts/deployContainer.sh
@@ -7,22 +7,18 @@ cd "$SCRIPT_DIR" || exit 1
 
 echo "This script does not build the container before deploying it. Whatever the current docker image on the system will be deployed to the server"
 
-cd ../artefacts
+echo Pushing container to Proxmox registry
 
-if [ ! -f webappdevkit.tar ]; then
-    podman save -o webappdevkit.tar webappdevkit:latest
-fi
+podman image tag webappdevkit:latest 192.168.1.253:5000/webappdevkit:latest
 
-echo Transfering container image.
-scp webappdevkit.tar myserver:containerImageImports
-echo Transfer complete.
+podman image push 192.168.1.253:5000/webappdevkit:latest --tls-verify=false
 
+echo Push complete.
 
-#echo Loading container image to local repository.
-#ssh myserver "sudo podman load -i ./containerImageImports/webappdevkit.tar"
+echo Loading container image from Proxmox registry.
 
-# run podman container 
-#ssh myserver "sudo podman run -p 80:80 localhost/webappdevkit:latest"
-
+# run podman container, replace old server if needed, will need to mount volume.
+export CONTAINER_NAME='webAppDevKitDev'
+ssh myserver "podman run -p 80:80 -d --name $CONTAINER_NAME --replace 192.168.1.253:5000/webappdevkit:latest"
 
 cd $current_dir
